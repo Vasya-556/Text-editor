4d9545ac71a713bd51fd8f2ae4ef08aef714cc59
Vasya-556 <vkobrin556@gmail.com>
2024-06-09 22:55:11 +0300

    add basic menu fields

--- textEditor.py
+++ textEditor.py
@@ -1,6 +1,15 @@
 from tkinter import *
 from tkinter import ttk
 
+def donothing():
+   ...
+
+def toggle_wrap():
+    if wrap_var.get():
+        text.config(wrap='word')
+    else:
+        text.config(wrap='none')
+
 root = Tk()
 root.title("Text Editor")
 root.geometry("600x300")
@@ -13,6 +22,25 @@ root.rowconfigure(0, weight=1)
 frm.columnconfigure(0, weight=1)
 frm.rowconfigure(0, weight=1)
 
+menubar = Menu(root)
+filemenu = Menu(menubar, tearoff=0)
+filemenu.add_command(label="New", command=donothing)
+filemenu.add_command(label="Open", command=donothing)
+filemenu.add_command(label="Save", command=donothing)
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command=root.quit)
+menubar.add_cascade(label="File", menu=filemenu)
+
+helpmenu = Menu(menubar, tearoff=0)
+helpmenu.add_command(label="Help", command=donothing)
+helpmenu.add_command(label="About", command=donothing)
+
+wrap_var = BooleanVar()
+wrap_var.set(True)
+
+helpmenu.add_checkbutton(label="Wrap button", onvalue=True, offvalue=False, variable=wrap_var, command=toggle_wrap)
+menubar.add_cascade(label="Help", menu=helpmenu)
+
 text = Text(frm, height = 5, width = 52, wrap='word')
 text.grid(column=0, row=0,sticky=(N, S, E, W))
 
@@ -21,4 +49,5 @@ scrollbar.grid(column=1,row=0, sticky=(N, S, E, W))
 
 text.config(yscrollcommand=scrollbar.set)
 
+root.config(menu=menubar)
 root.mainloop()
